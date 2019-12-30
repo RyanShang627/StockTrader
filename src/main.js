068cbcd5236ce16ed3f72c79a7284bd95181c1e8
RyanShang627 <ryan.d.shang@gmail.com>
2019-12-29 22:55:16 -0800

    chore: Import and use vue-resource

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from "./App.vue";
 import { routes } from "./routes.js";
 
 import store from './store/store.js'
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-fc9d5.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString()
